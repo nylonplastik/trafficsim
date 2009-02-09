e7804785c169021f69ddb9c0cf3ff2c3ad3b2f35
Mariusz Ceier <mceier@gmail.com>
2009-02-09 23:58:05 +0100

    Rename, and disable menu item
    Renamed TrafficSimFrame to TrafficSimMainFrame
    Disabled "File->Map editor" menu item, and added tooltip that this is
    not implemented yet.
    
    Signed-off-by: Mariusz Ceier <mceier@gmail.com>

---
+++ simulation/src/gui/TrafficSimMainFrame.java
@@ -0,0 +1,124 @@
+/*
+    TrafficSim is simulation of road traffic
+    Copyright (C) 2009  Mariusz Ceier, Adam Rutkowski
+
+    This file is part of TrafficSim
+
+    TrafficSim is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TrafficSim is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TrafficSim.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package gui;
+
+/* Imports {{{ */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/* }}} */
+
+/**
+ * Main frame
+ *
+ * @class TrafficSimFrame
+ *
+ */
+@SuppressWarnings("serial")
+public class TrafficSimMainFrame
+	extends JFrame
+    implements ActionListener, ComponentListener
+{
+	private JMenuItem quit_menu_item = null;
+	private JMenuItem map_editor_menu_item = null;
+	
+	/**
+	 * Initializes frame
+	 */
+	protected void initFrame()
+    {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JMenuBar menu_bar = new JMenuBar();
+        JMenu file_menu = new JMenu("File");
+        file_menu.setMnemonic(KeyEvent.VK_F);
+        menu_bar.add(file_menu);
+        map_editor_menu_item = new JMenuItem("Map Editor...", KeyEvent.VK_M);
+        map_editor_menu_item.setAccelerator(
+        		KeyStroke.getKeyStroke(KeyEvent.VK_M,ActionEvent.CTRL_MASK));
+        map_editor_menu_item.addActionListener(this);
+        file_menu.add(map_editor_menu_item);
+        file_menu.addSeparator();
+        quit_menu_item = new JMenuItem("Quit", KeyEvent.VK_Q);
+        quit_menu_item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, 
+        		ActionEvent.CTRL_MASK));
+        quit_menu_item.addActionListener(this);
+        file_menu.add(quit_menu_item);
+        setJMenuBar(menu_bar);
+        setEnabled(true);
+        setResizable(true);
+        setUndecorated(true);
+        // TODO: is this really needed ? ... 
+        // TODO: must test this under more conformant wm ... 
+        // addComponentListener(this);
+        setContentPane(new TrafficSimMainPanel());
+    }
+	
+    public TrafficSimMainFrame() throws HeadlessException {
+		super();
+		initFrame();
+	}
+
+	public TrafficSimMainFrame(GraphicsConfiguration gc) {
+		super(gc);
+		initFrame();
+	}
+
+	public TrafficSimMainFrame(String title, GraphicsConfiguration gc) {
+		super(title, gc);
+		initFrame();
+	}
+
+	public TrafficSimMainFrame(String title) throws HeadlessException {
+		super(title);
+		initFrame();
+	}
+
+    public void actionPerformed(ActionEvent e)
+    {
+        if (e.getSource() == quit_menu_item)
+        {
+            this.dispose();
+        }
+
+    }
+
+	@Override
+	public void componentHidden(ComponentEvent e) {
+	}
+
+	@Override
+	public void componentMoved(ComponentEvent e) {		
+	}
+
+	@Override
+	public void componentResized(ComponentEvent e) {
+		doLayout();
+	}
+
+	@Override
+	public void componentShown(ComponentEvent e) {
+	}
+
+}
+
+/* vim: set ts=4 sts=4 sw=4 expandtab foldmethod=marker : */
