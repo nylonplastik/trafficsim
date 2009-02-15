673ba04d8f2222655db8c8385217233e9399cb55
Mariusz Ceier <mceier@gmail.com>
2009-02-15 12:34:37 +0100

    PacketTypes
    Signed-off-by: Mariusz Ceier <mceier@gmail.com>

---
+++ simulation/src/trafficsim/network/PacketTypes.java
@@ -0,0 +1,36 @@
+/*
+    TrafficSim is simulation of road traffic
+    Copyright (C) 2009  Mariusz Ceier, Adam Rutkowski
+
+    This file is part of TrafficSim
+
+    TrafficSim is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TrafficSim is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TrafficSim.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package trafficsim.network;
+
+public class PacketTypes
+{
+	public static final int LOGIN_TYPEID = 0;
+	public static final int STATS_TYPEID = 1;
+	public static final int SET_ROUTE_TYPEID = 2;
+	public static final int UPDATE_TYPEID = 3;
+	public static final int GET_ID_TYPEID = 4;
+	public static final int CHANGE_ACCELERATION_TYPEID = 5;
+	public static final int GET_MODEL_TYPEID = 6;
+	public static final int SPAWN_CAR_TYPEID = 7;
+	public static final int CHANGE_LIGHTS_STATE_TYPEID = 8;	
+}
+
+/* vim: set ts=4 sts=4 sw=4 expandtab foldmethod=marker : */
